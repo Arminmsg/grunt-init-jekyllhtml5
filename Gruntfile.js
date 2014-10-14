b52da0621dc5798d7caa042ff018d319699bdc3e
Armin <arminmesic@hotmail.de>
2014-10-14 02:16:04 +0200

    Auto build if something changes

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            }
+        },
+        sass: {
+          dist: {
+            options: {
+              style: 'compressed'
+            },
+            files: {
+              'css/alter.css': 'css/main.scss',
+            }
+          }
+        },
+        watch: {
+            options: {
+                livereload: true,
+                interrupt: true,
+                atBegin: true,
+              
+            },
+            html: {
+                files: [
+                    "_includes/*.html",
+                    "_layouts/*.html",
+                    "_posts/*.markdown",
+                    "_config.yml",
+                    "index.html"
+                ], 
+                tasks: ["shell:jekyllBuild"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-shell");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+
+    grunt.registerTask("default", ["shell"]);
+};
